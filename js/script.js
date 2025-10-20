31004f25f1148d12221f45f46b0156314ead579a
setyodwinugroho11 <setyodwinugroho115@gmail.com>
2025-10-20 22:26:29 +0700

    jarvis3

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // toggle class active
 const navbarNav = document.querySelector(".navbar-nav");
 //ketika menu di klik
@@ -13,19 +12,3 @@ document.addEventListener("click", (e) => {
     navbarNav.classList.remove("active");
   }
 });
-=======
-// toggle class active
-const navbarNav = document.querySelector(".navbar-nav");
-//ketika menu di klik
-document.querySelector("#menu").onclick = () => {
-  navbarNav.classList.toggle("active");
-};
-
-// klik di luar sidebar untuk menghilangkan navbar
-const menu = document.querySelector("#menu");
-document.addEventListener("click", (e) => {
-  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
-    navbarNav.classList.remove("active");
-  }
-});
->>>>>>> 13a3ca583e88efdad949d1c7a7da11586d6c3714
